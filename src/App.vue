530b446a1991f0df1d18475e8d0d0a4a0542b480
Alex <xinfoz19@gmail.com>
2023-05-30 01:26:42 +0200

    start project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Hi! It is working!</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
